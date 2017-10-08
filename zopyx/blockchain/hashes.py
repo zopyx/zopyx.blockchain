2896d64cd75b1020b298d80c83783cfd94623284
Andreas JungÂ <info@zopyx.com>
2017-10-08 16:01:39 +0200

    updated

--- zopyx/blockchain/hashes.py
+++ zopyx/blockchain/hashes.py
@@ -1,7 +1,9 @@
-
+import os
 import fs
 import fs.opener
+import tempfile
 import hashlib
+import lxml.etree
 
 
 def hashes_for_fs(url):
@@ -11,9 +13,24 @@ def hashes_for_fs(url):
 
     result = list()
     for name in walker.files():
+
         s256 = hashlib.sha256()
-        with handle.open(name, 'rb') as fp:
-            s256.update(fp.read())
+
+        # normalized XML
+        if name.endswith(('.xml', '.XML')):
+            with handle.open(name, 'rb') as fp:
+                root= lxml.etree.parse(fp)
+                tmpf = tempfile.mktemp(suffix='.xml')
+                with open(tmpf, 'wb') as fp_out:
+                    root.write_c14n(fp_out)
+                with open(tmpf, 'rb') as fp:
+                    s256.update(fp.read())
+                os.unlink(tmpf)
+        else:
+            # default
+            with handle.open(name, 'rb') as fp:
+                s256.update(fp.read())
+
         result.append(dict(
             name=name,
             hash=s256.hexdigest(),
@@ -23,7 +40,4 @@ def hashes_for_fs(url):
 if __name__ == '__main__':
     import sys
     import pprint
-
     pprint.pprint(hashes_for_fs(sys.argv[-1]))
-
-
